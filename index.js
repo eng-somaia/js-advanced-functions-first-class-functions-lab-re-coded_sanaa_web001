9e04e535bb3be9cc6b785e3a5732c20eadd75174
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-08-20 03:05:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let drivers = ['Tom', 'John', 'Pale', 'Susan']
 const returnFirstTwoDrivers = function (drivers){
-   return drivers[0] +" "+ drivers[1];
-
+   x= [drivers[0], drivers[1]]
+return x;
   }
-}
